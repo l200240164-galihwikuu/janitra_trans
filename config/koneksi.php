68847cbc153f8aa11eeb7ab6e6e173de9b829fd7
galihwikuu <l200240164@student.ums.ac.id>
2026-06-06 01:50:39 +0700

    Update database connection settings in koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,10 +1,10 @@
 <?php
 
-define('DB_HOST', 'acela.proxy.rlwy.net');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
 define('DB_PASS', 'xapNcEqwGUVxXVmZLlODhEBCWbcpbJRX');
 define('DB_NAME', 'railway');
-define('DB_PORT', 14270);
+define('DB_PORT', 3306);
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 if (!$conn) {
