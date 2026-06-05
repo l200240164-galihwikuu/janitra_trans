e7d28dfd1033e0059639e85b88dcc09f398a07fc
galihwikuu <l200240164@student.ums.ac.id>
2026-06-06 01:12:43 +0700

    Update database connection settings in koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'janitra_surya');
+define('DB_PASS', 'xapNcEqwGUVxXVmZLlODhEBCWbcpbJRX');
+define('DB_NAME', 'railway');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$conn) {
