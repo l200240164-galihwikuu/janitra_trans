a10e9922cdc8863d7dda0643ef7a4c08a1abd49b
Galih Wiku <l200240164@student.ums.ac.id>
2026-06-06 15:12:24 +0700

    Update include path for database connection

--- public/admin/data-booking.php
+++ public/admin/data-booking.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     header('Location: login.php'); exit;
 }
 
-include '../config/koneksi.php';
+include '../../config/koneksi.php';
 $halaman_admin = 'booking';
 $pesan = ''; $tipe = '';
 
